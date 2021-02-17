74fe44a7eaa7b52686a5bd555494b367f7074b67
sgalella <santi.galella@gmail.com>
2021-02-17 21:39:01 +0100

    Add __main__.py

---
+++ popcorn_agent/__main__.py
@@ -0,0 +1,13 @@
+from .agent import PopCornAgent
+
+# Set voice to true if you want to hear the agent. Set keyboard to false if
+# you prefer talking instead of typing.
+popcorn = PopCornAgent(voice=False, keyboard=True)
+popcorn.speak("Hi, I'm PopCorn. "
+              "What kind of film do you want to watch?")
+while not popcorn.end:
+    user_message = popcorn.listen()  # TODO: If intelligible, ask again
+    popcorn.parser(user_message)
+    popcorn.name_managing()
+    popcorn.consult_ontology()
+    popcorn.response_generator()
